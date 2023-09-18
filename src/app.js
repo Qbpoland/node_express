139406d25f8f0314b13f40b6428d7e6400f2daab
Qbpoland <jakub.stasiuk2005@gmail.com>
2023-09-18 13:07:12 +0200

    init

---
+++ src/app.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const path = require('path');
+const hbs = require('hbs');
+
+
+const app = express();
+
+
+app.set('views', './src/views');
+hbs.registerPartials(path.join(__dirname, 'views/partials'));
+app.set('view engine', 'hbs');
+
+app.get('/', (req, res) => {
+    res.render('index', {
+        pageTitle: 'Strona główna',
+    });
+
+});
+app.get('/omnie', (req, res) => {
+    res.render('omnie', {
+        pageTitle: 'Strona O mnie',
+    });
+
+});
+
+app.get('/kontakt', (req, res) => {
+    res.render('kontakt',  {
+        pageTitle: 'Strona kontakt',
+    });
+
+});
+
+
+app.listen(5000, () => {
+    console.log('Serwer działa!');
+
+});
